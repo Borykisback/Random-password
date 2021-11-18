8eef9653a094b821a95a01407f5564e424fd699e
Borykisback <bokback@mail.ru>
2021-11-18 21:33:08 +0300

    Add saved passwords

--- random_password.py
+++ random_password.py
@@ -2,17 +2,23 @@ import string
 import random
 
 chars = string.ascii_letters + string.digits
-print("Рандомный пароль V1.0")
+print("Рандомный пароль V1.0\nМинимальная длина пароля 6 символов\nМаксимальная длина пароля 128 символов")
 while True:
     try:
         length = int(input("Длина пароля: "))
         if not 6 <= length <= 128:
             raise ValueError()
-    except ValueError:
-        print("Максимальная длина пароля 128 символов\nМинимальная длина пароля 6 символов")
-    except:
-        print("Ошибка! Попробуйте снова")
-    else:
         break
+    except ValueError:
+        print("Ошибка! Попробуйте снова")
+
 password = (''.join([random.choice(chars) for i in range(length)]))
 print(f"Пароль: {password}")
+
+save_input = input("Дайте название паролю: ")
+
+with open('save.txt', 'r') as read_file:
+    data = read_file.read()
+
+with open('save.txt', 'w') as save:
+    save.write(data + "\n" + password + " - " + save_input)
